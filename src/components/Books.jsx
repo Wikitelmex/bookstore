7acb2e00701d2828548d0224388d646710086876
Alex Castillo <wikitelmex@gmail.com>
2021-11-12 20:08:21 -0700

    Booklist
    - add a booklist with mock/redux data
    - add a remove book button
    - add a form to add an book

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Books = () => {
-  const data = useSelector(state => state.books);
-  
+  const data = useSelector((state) => state.books);
+
   return (
     <div>
       <h1>
         Books
       </h1>
       <ul>
-        {data.books.map(book => (
+        {data.books.map((book) => (
           <li key={book.id}>
             <span>title: {book.title}</span>
             <span>author: {book.author} </span>
-            <button id={book.id}>Delete Book {book.id}</button>
+            <button type="button" id={book.id}>Delete Book {book.id}</button>
           </li>
         ))}
       </ul>
+      <form action="">
+        <label htmlFor="title">
+          Title
+          <input type="text" name="title" id="title" />
+        </label>
+        <label htmlFor="author">
+          Author
+          <input type="text" name="author" id="author" />
+        </label>
+        <button type="submit">Add Book</button>
+      </form>
     </div>
   );
 };

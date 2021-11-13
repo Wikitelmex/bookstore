705e1ce88f7c5dc12d68f265b2d68d3c6f27895d
Alex Castillo <wikitelmex@gmail.com>
2021-11-13 10:49:40 -0700

    Books CRUD
    - add addBook feature
    - add removeBook feature
    - implement deletebook button
    - implement addbook inputs and button
    - installed/used GUID generator
    - implement middleware LOGGER

--- src/redux/books/bookReducer.js
+++ src/redux/books/bookReducer.js
@@ -7,12 +7,12 @@ import {
 const initialState = {
   books: [
     {
-      id: 1,
+      id: '1',
       title: 'Book 1',
       author: 'Author 1',
     },
     {
-      id: 2,
+      id: '2',
       title: 'Book 2',
       author: 'Author 2',
     },

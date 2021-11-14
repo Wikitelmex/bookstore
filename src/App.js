14850d8d2a1045d86f2077cf31d77199c830f39c
Alex Castillo <wikitelmex@gmail.com>
2021-11-13 18:56:17 -0700

    styling corrections

--- src/App.js
+++ src/App.js
@@ -21,20 +21,25 @@ function App() {
     <Provider store={store}>
       <Router>
         <div className="m-3">
-        
+
           <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
             <div className="container-fluid ms-5">
-              <Link className="navbar-brand text-primary h1" to="/">Bookstore CMS</Link>
+
+              <Link className="navbar-brand text-primary h1" to="/">
+                <h3 className='fw-bold mt-1'>
+                  Bookstore CMS
+                </h3>
+              </Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
               </button>
-              <div className="collapse navbar-collapse ms-auto" id="navbarNavAltMarkup">
+              <div className="collapse navbar-collapse mb-2 ms-auto" id="navbarNavAltMarkup">
                 <div className="navbar-nav me-auto text-light">
                   <Link to="/" className="nav-link">Books</Link>
                   <Link to="/categories" className="nav-link">Categories</Link>
                 </div>
-                <div className="navbar-nav ms-auto text-light">
-                  <Link to="/" className="nav-link">carita</Link>
+                <div className="navbar-nav ms-auto me-4 text-primary">
+                  <h2 class="bi bi-person-circle"></h2>
                 </div>
               </div>
             </div>
@@ -43,15 +48,16 @@ function App() {
           <br />
           <br />
           <br />
+          <br />
           <div>
-          <Switch className="bg-dark">
-            <Route path="/categories">
-              <Categories />
-            </Route>
-            <Route path="/">
-              <Books />
-            </Route>
-          </Switch>
+            <Switch className="bg-dark">
+              <Route path="/categories">
+                <Categories />
+              </Route>
+              <Route path="/">
+                <Books />
+              </Route>
+            </Switch>
           </div>
         </div>
       </Router>
